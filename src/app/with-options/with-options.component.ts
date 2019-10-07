e7c9236e65c32930afe6a20cd609bcc8ffb8775b
Crisoforo Lopez <43754109+chr1soscl@users.noreply.github.com>
2019-10-06 21:56:54 -0400

    1.-Selection extension.

--- src/app/with-options/with-options.component.ts
+++ src/app/with-options/with-options.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WithOptionsComponent implements OnInit {
 
-  dtOptions: DataTables.Settings = {};
+  dtOptions: DataTables.Settings = {
+
+  };
 
   ngOnInit(): void {
     this.dtOptions = {
       pagingType: 'full_numbers',
-      columnDefs: [{ targets: 3, type: 'date' }]
+      columnDefs: [{ targets: 3, type: 'date' }, {
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0
+      }],
+      select: { style: 'multi' },
+      order: [[1, 'asc']]
+
     };
   }
 
